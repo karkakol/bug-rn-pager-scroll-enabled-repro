a0dd4d1e2552ccaf35c770b0521f965d749def2f
Karol Kąkol <kakol.karol00@gmail.com>
2025-07-14 17:57:19 +0200

    ios bug repro

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,19 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useRef, useState } from 'react';
+import { View } from 'react-native';
+import PagerView from 'react-native-pager-view';
 
 export default function RootLayout() {
+
+  const viewPagerRef = useRef<PagerView | null>(null);
+    const [currentPageIndex, setCurrentPageIndex] = useState(0);
+
+
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -19,11 +26,17 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <PagerView 
+      style={{flex: 1}} 
+      scrollEnabled={false}>
+        {["red", "green", "blue"].map((color ) =>{
+          const style ={backgroundColor: color, flax: 1}
+          return (
+            <View style={style} key={color} />
+          )
+        })}
+
+      </PagerView>
     </ThemeProvider>
   );
 }
