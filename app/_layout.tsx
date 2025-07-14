03e4044587537bb81176bcb56787acfc9247be5e
Karol Kąkol <kakol.karol00@gmail.com>
2025-07-14 17:58:44 +0200

    cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { useRef, useState } from 'react';
 import { View } from 'react-native';
 import PagerView from 'react-native-pager-view';
 
 export default function RootLayout() {
-
-  const viewPagerRef = useRef<PagerView | null>(null);
-    const [currentPageIndex, setCurrentPageIndex] = useState(0);
-
-
-
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
@@ -30,7 +14,7 @@ export default function RootLayout() {
       style={{flex: 1}} 
       scrollEnabled={false}>
         {["red", "green", "blue"].map((color ) =>{
-          const style ={backgroundColor: color, flax: 1}
+          const style ={backgroundColor: color, flex: 1}
           return (
             <View style={style} key={color} />
           )
